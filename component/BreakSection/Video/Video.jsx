7ab45eb0513eae4d89a9312a13ac28184c3555f1
Gurpreet <dhoat30@gmail.com>
2023-11-19 08:36:01 +1300

    unmuted the video

--- component/BreakSection/Video/Video.jsx
+++ component/BreakSection/Video/Video.jsx
@@ -45,7 +45,7 @@ export default function Video({
           <ReactPlayer
             url={videoFile.url}
             loop={true}
-            muted={true}
+            muted={false}
             playing={true}
             onReady={handleVideoReady}
             style={{
